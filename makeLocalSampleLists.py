0b79f388adf9c4f56cae2d478b4372a8ad5ad243
anovak10 <novak.andrzej@gmail.com>
2020-05-26 15:53:46 +0200

    wip

--- makeLocalSampleLists.py
+++ makeLocalSampleLists.py
@@ -1,18 +1,18 @@
 import os
 
-dirs = ['Hcc', 'Hbb', "qcd"]
-dirs = ['Z']
+# dirs = ['Hcc', 'Hbb', "qcd"]
+dirs = ['qcd']
+maind = '/net/scratch_cms3a/novak/94x_samples/'
 for d in dirs:
-	tests = os.listdir(d+"/test")
-	f = open("list_test_{}.txt".format(d), "w")
-	for line in tests:
-		f.write(d+"/test/"+line+"\n")
-	f.close()
-	
-	trains = os.listdir(d)
-	f = open("list_{}.txt".format(d), "w")
-	for line in trains:
-		if not line.endswith(".root"): continue
-		f.write(d+"/"+line+"\n")
-	f.close()
-	
+    tests = os.listdir(maind + d + "/test")
+    f = open("list_test_{}.txt".format(d), "w")
+    for line in tests:
+        f.write(maind + d + "/test/" + line + "\n")
+    f.close()
+
+    trains = os.listdir(maind + d)
+    f = open("list_{}.txt".format(d), "w")
+    for line in trains:
+        if not line.endswith(".root"): continue
+        f.write(maind + d + "/" + line + "\n")
+    f.close()
--- makeRemoteSampleLists.py
+++ makeRemoteSampleLists.py
@@ -1,84 +1,71 @@
 import os
 from argparse import ArgumentParser
-import ROOT 
 
 #parser = ArgumentParser()
 #arser.add_argument("--include", help="Path to training sample", default=)
 #args=parser.parse_args()
 #print(args.include)
+list_dir = 'lists_pre'
+f_dir = '102RE'
 
-
-#names = ["qcd", "Hcc", "Hbb", "gg", "Z"]
-names = ["Hcc", 'qcd', 'Hbb']
+#names = ["QCD", "Glu", "Bul", "GluGluH"]
 #names = ["GluGluH"]
 #names = ["Hcc", "Hbb", "Z"]
-#names = ["Zqq"]
+#names = ["Hbb"]
+names = ["multimass", 'ggHbb', 'ggHcc', "qcd"]
 
-dcap = "dcap://grid-dcap-extern.physik.rwth-aachen.de/pnfs/physik.rwth-aachen.de/cms/store/user/anovak/tag_comp_18"
-srm = "srm://grid-srm.physik.rwth-aachen.de:8443/srm/managerv2?SFN=/pnfs/physik.rwth-aachen.de/cms/store/user/anovak/tag_comp_18"
+dcap = "dcap://grid-dcap-extern.physik.rwth-aachen.de/pnfs/physik.rwth-aachen.de/cms/store/user/anovak/{}".format(f_dir)
+srm = "srm://grid-srm.physik.rwth-aachen.de:8443/srm/managerv2?SFN=/pnfs/physik.rwth-aachen.de/cms/store/user/anovak/{}".format(f_dir)
 fnal = False
-if fnal:dirs = os.popen("eos root://cmseos.fnal.gov ls /store/group/lpchbb/20180524_ak8_94x/").read().split("\n")
-#else:	dirs = os.popen("gfal-ls "+srm).read().split("\n")
-else:	dirs = os.popen("gfal-ls "+srm).read().split("\n")
+
+dirs = os.popen("gfal-ls " + srm).read().split("\n")
+#else:	dirs = os.popen("lcg-ls "+srm).read().split("\n")
 dirs = [d.split("/")[-1] for d in dirs]
 
-print "Processsing directories starting with: ", names
-print "======================================="
+print("Processsing directories starting with: ", names)
+print("=======================================")
 all_files = []
-print dirs
+print(dirs)
 for i, d in enumerate(dirs):
-	#print i, d
-	dir_files = []
-	go = False
-	for name in names:
-		if d.startswith(name): go = True
-	if not go: continue
-	print d
-	if fnal:
-		sds = os.popen("eos root://cmseos.fnal.gov ls /store/group/lpchbb/20180524_ak8_94x/"+d).read().split("\n")
-		for sd in sds:
-			if not sd.endswith(tuple(["madgraph", "pythia8"])):continue 
-			sd2 = os.popen("eos root://cmseos.fnal.gov ls /store/group/lpchbb/20180524_ak8_94x/"+d+"/"+sd).read().split("\n")[0]
-			sd3 = os.popen("eos root://cmseos.fnal.gov ls /store/group/lpchbb/20180524_ak8_94x/"+d+"/"+sd+"/"+sd2).read().split("\n")[0]
-			sd4 = os.popen("eos root://cmseos.fnal.gov ls /store/group/lpchbb/20180524_ak8_94x/"+d+"/"+sd+"/"+sd2+"/"+sd3).read().split("\n")[0]
-			files = os.popen("eos root://cmseos.fnal.gov ls /store/group/lpchbb/20180524_ak8_94x/"+d+"/"+sd+"/"+sd2+"/"+sd3+"/"+sd4).read().split("\n")
-			path = "root://cmseos.fnal.gov//store/group/lpchbb/20180524_ak8_94x/"+d+"/"+sd+"/"+sd2+"/"+sd3+"/"+sd4+"/"
-			files = [path+f for f in files if f.endswith("root")]
-	
-			with open("2017lists/"+sd+".txt", 'w') as f:
-				        f.write('\n'.join(files))
-	
-	else:
-		sd = os.popen("gfal-ls "+srm+"/"+d).read().split('\n')
-		#sd = os.popen("lcg-ls "+"srm://grid-srm.physik.rwth-aachen.de:8443/srm/managerv2?SFN=/pnfs/physik.rwth-aachen.de/cms/store/user/anovak/94xv5"+"/"+d).read().split('\n')
-		for s in sd:
-			print s
-			if not s.endswith(tuple(["madgraph", "pythia8", "correctedcfg"])):continue 
-			fs = []
-			sd2 = os.popen("gfal-ls "+srm+"/"+d+"/"+s).read().replace("\n", "")
-			sd3 = os.popen("gfal-ls "+srm+"/"+d+"/"+s+"/"+sd2).read().split("\n")[0]
-			sd4 = os.popen("gfal-ls "+srm+"/"+d+"/"+s+"/"+sd2+"/"+sd3).read().replace("\n", "")
-			files = os.popen("gfal-ls "+srm+"/"+d+"/"+s+"/"+sd2+"/"+sd3+'/'+sd4).read().split("\n")
-			path = dcap+"/"+d+"/"+s+"/"+sd2+"/"+sd3+"/"+sd4
-			for f in files:
-				if not f.endswith("root"): continue
-				fs.append(path+"/"+f)
-			print "gfal-ls "+srm+"/"+d+"/"+s+"/"+sd2+"/"+sd3+'/'+sd4
-			print files
-			s = s.replace("/pnfs/physik.rwth-aachen.de/cms", "")
-			#srm = srm.replace(".de/cms/store/user/anovak/94xv5", ".de/cms")
-			#sd2 = os.popen("lcg-ls "+srm+s).read().replace("\n", "").replace("/pnfs/physik.rwth-aachen.de/cms", "")
-			#print sd2
-			#sd3 = os.popen("lcg-ls "+srm+sd2).read().replace("\n", "").replace("/pnfs/physik.rwth-aachen.de/cms", "")
-			#sd4 = os.popen("lcg-ls "+srm+sd3).read().replace("\n", "").replace("/pnfs/physik.rwth-aachen.de/cms", "")
-			#files = os.popen("lcg-ls "+srm+sd4).read().split("\n")
-			#files = [f.replace("/pnfs/physik.rwth-aachen.de/cms", "") for f in files]
-			#for f in files:
-			#	if len(f) < 5: continue
-			#	if not f.endswith("root"): continue
-			#	fs.append("dcap://grid-dcap-extern.physik.rwth-aachen.de/pnfs/physik.rwth-aachen.de/cms/"+f)
-			#s = s.split("/")[-1]
-			#print s
-			
-			with open("lists/"+s+".txt", 'w') as f:
-			        f.write('\n'.join(fs))
+    dir_files = []
+    go = False
+    for name in names:
+        if d.startswith(name): go = True
+    if not go: continue
+    if True:
+        sd = os.popen("gfal-ls " + srm + "/" + d).read().split('\n')
+        #sd = os.popen("lcg-ls "+"srm://grid-srm.physik.rwth-aachen.de:8443/srm/managerv2?SFN=/pnfs/physik.rwth-aachen.de/cms/store/user/anovak/94xv3"+"/"+d).read().split('\n')
+        for s in sd:
+            if not s.endswith(tuple(["madgraph", "pythia8"])): continue
+            fs = []
+            #print s
+            sd2 = os.popen("gfal-ls " + srm + "/" + d + "/" +
+                           s).read().replace("\n", "")
+            sd3 = os.popen("gfal-ls " + srm + "/" + d + "/" + s + "/" +
+                           sd2).read().replace("\n", "")
+            sd4 = os.popen("gfal-ls " + srm + "/" + d + "/" + s + "/" + sd2 +
+                           "/" + sd3).read().replace("\n", "")
+            files = os.popen("gfal-ls " + srm + "/" + d + "/" + s + "/" + sd2 +
+                             "/" + sd3 + '/' + sd4).read().split("\n")
+            path = dcap + "/" + d + "/" + s + "/" + sd2 + "/" + sd3 + "/" + sd4
+            for f in files:
+                if not f.endswith("root"): continue
+                fs.append(path + "/" + f)
+            #print "gfal-ls "+srm+"/"+d+"/"+s+"/"+sd2+"/"+sd3+'/'+sd4
+            #print files
+            #s = s.replace("/pnfs/physik.rwth-aachen.de/cms", "")
+            #srm = srm.replace(".de/cms/store/user/anovak/94xv3", ".de/cms")
+            #sd2 = os.popen("lcg-ls "+srm+s).read().replace("\n", "").replace("/pnfs/physik.rwth-aachen.de/cms", "")
+            #sd3 = os.popen("lcg-ls "+srm+sd2).read().replace("\n", "").replace("/pnfs/physik.rwth-aachen.de/cms", "")
+            #sd4 = os.popen("lcg-ls "+srm+sd3).read().replace("\n", "").replace("/pnfs/physik.rwth-aachen.de/cms", "")
+            #files = os.popen("lcg-ls "+srm+sd4).read().split("\n")
+            #files = [f.replace("/pnfs/physik.rwth-aachen.de/cms", "") for f in files]
+            #for f in files:
+            #	if len(f) < 5: continue
+            #	if not f.endswith("root"): continue
+            #	fs.append("dcap://grid-dcap-extern.physik.rwth-aachen.de/pnfs/physik.rwth-aachen.de/cms/"+f)
+            #s = s.split("/")[-1]
+            #print fs
+
+            with open(list_dir+"/" + s + ".txt", 'w') as f:
+                f.write('\n'.join(fs))
